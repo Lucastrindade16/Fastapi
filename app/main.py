baf096ca414760fc5c5da66d02ba3502c54eb936
Lucas Trindade de Santana <159261499+Lucastrindade16@users.noreply.github.com>
2025-06-02 08:26:27 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,8 @@ from routes import ator_serie, avaliacao_serie, categoria, serie, motivos_assist
 app = FastAPI()
 
 app.include_router(serie.router, prefix="/series", tags=["Séries"])
-app.include_router(delete.router, prefix="/delete", tags=["Delete"])
 app.include_router(categoria.router, prefix="/categorias", tags=["Categorias"])
 app.include_router(ator_serie.router, prefix="/atores", tags=["Atores"])
 app.include_router(avaliacao_serie.router, prefix="/avaliacoes", tags=["Avaliações"])
 app.include_router(motivos_assistir.router, prefix="/motivos", tags=["Motivos para Assistir"])
-app.include_router(update.router, prefix="/update", tags=["Update"])
+app.include_router(delete.router, prefix="/delete", tags=["Delete"])
